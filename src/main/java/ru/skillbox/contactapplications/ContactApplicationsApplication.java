c7058e80198965f7d786ac7039afa51427958a38
Nicko404 <kolsak2013@yandex.ru>
2024-01-16 20:18:18 +0300

    Initial commit

---
+++ src/main/java/ru/skillbox/contactapplications/ContactApplicationsApplication.java
@@ -0,0 +1,13 @@
+package ru.skillbox.contactapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactApplicationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactApplicationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skillbox/contactapplications/ContactApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.contactapplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApplicationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
